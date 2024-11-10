041f92e3726b1fa6271cebd075af773595a55b1d
JoaoOliveiraZz <joaolima.gxp@gmail.com>
2024-11-09 22:14:37 -0300

    :tada: Initial commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import { fastify, FastifyInstance } from 'fastify';
+
+
+
+const app: FastifyInstance = fastify();
+
+app.listen({
+    port: 3100
+}, () => {
+    console.log("Server is running...");
+})
+
+app.get("/hello", () => {
+    return "Bem vindo ao nosso servidor, amigo!"
+})
