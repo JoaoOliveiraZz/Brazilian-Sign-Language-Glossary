a0b67d3366ef765bf6596f311eef179c07cd1fcf
JoaoOliveiraZz <joaolima.gxp@gmail.com>
2024-11-10 00:09:16 -0300

    Creating categories table

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,34 @@
+import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
+import {z} from 'zod'
 
 const app = fastify();
 
-app.get("/", () => {
-    return "Bem vindo ao nosso servidor!"
+const prisma = new PrismaClient();
+
+app.get("/category", async () => {
+    const categories = await prisma.category.findMany();
+    return categories;
+})
+
+app.post("/category", async (request, reply) => {
+
+    const createCategorySchema = z.object({
+        name: z.string(),
+        description: z.string()
+    });
+
+    const {name, description} = createCategorySchema.parse(request.body);
+
+    await prisma.category.create({
+        data: {
+            name: name,
+            description: description
+        }
+    })
+
+    return reply.status(201).send();
+
 })
 
 app.listen({
